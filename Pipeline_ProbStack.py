d85bb25b53a64bcd57bbfb753f2826333bcf6ad9
ym3141 <yma2@caltech.edu>
2019-05-28 21:51:08 -0700

    Finished Pipeline_ProbStack.py

--- Pipeline_ProbStack.py
+++ Pipeline_ProbStack.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 from multiprocessing import Pool
 from os import listdir, mkdir
+from functools import partial
 
 import h5py
 import numpy as np
@@ -43,27 +44,29 @@ def runIlastik(ilastikProject, files):
     subprocess.run(cmd)
 
 
-def stackProb(dic):
-    probRe = re.compile(r'(w\d[^ _]+_s\d+)_frame(\d+)_\w+.h5')
-    posDict = dict()
+def stackProb(dic, posID, output):
+    probRe = re.compile(r'(w\d[^ _]+_{0})_frame(\d+)_\w+.h5'.format(posID))
+    probList = []
     for name in listdir(dic):
         if probRe.match(name):
-            match = probRe.search(name)
+            probList.append(name)
 
-            if not match.group(1) in posDict:
-                posDict[match.group(1)] = []
-            
-            posDict[match.group(1)].append(name)
+    probList.sort(key=(lambda name : probRe.search(name).group(2)))
+    arrList = []
+    for maskName in probList:
+        h5file = h5py.File('{0}/{1}'.format(dic, maskName))
+        probMask = np.array(h5file['exported_data'])[:, :, 0]
+        arrList.append((probMask * 255).astype('uint8'))
 
-    for pos in posDict:
-        posDict[pos].sort(key=(lambda name : probRe.search(name).group(2)))
-        arrList = []
-        for maskName in posDict[pos]:
-            h5file = h5py.File('{0}/{1}'.format(dic, maskName))
-            probMask = np.array(h5file['exported_data'])[:, :, 0]
-            arrList.append((probMask * 255).astype('uint8'))
+    save2stack(arrList, '{0}/{1}_prob.tiff'.format(output, posID))
 
-        save2stack(arrList, '{0}/{1}_prob.tiff'.format(dic, pos))
+def mainPipe(posID, stackDict, ilastikProj, tempDir, oDir):
+    print('Processing position {0}...'.format(posID))
+    splitImgs = unpackStack(stackDict[posID][0], tempDir)
+    runIlastik(ilastikProj, splitImgs)
+    stackProb(tempDir, posID, oDir)
+
+
 
 
 if __name__ == '__main__':
@@ -74,18 +77,22 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--Project", help="trained ilastik project")
     parser.add_argument("-o", "--Output", help="The directory for csv output")
     parser.add_argument("-t", "--MultiThread", type=int, default=1, help="Number of thread to use, default = 1")
-    parser.add_argument("--temp", default='./pepeline_temp', help="The directory for temp files")
+    parser.add_argument("--temp", default='./pipeline_temp', help="The directory for temp files")
 
     args = parser.parse_args()
 
     wDic = args.WorkDic
-    ilastikProject = args.Project
+    ilastikProject = ospath.normpath(args.Project)
+    print(ilastikProject)
 
     if args.Output:
-        oDic = args.Output
+        oDir = ospath.normpath(args.Output)
     else:
-        oDic = wDic + '/pepline1Output'
-    print("Output to " + oDic)
+        oDir = ospath.normpath(wDic + '/pipeline_probStack_output')
+    if not ospath.exists(oDir):
+        mkdir(oDir)
+
+    print("Output to " + oDir)
 
     mtNum = args.MultiThread
     if mtNum == 1:
@@ -101,9 +108,18 @@ if __name__ == '__main__':
         mkdir(tempPath)
 
     stackPairDict = generatStackList(wDic)
+    if multiP:
+        print("Starting {0} threads......".format(mtNum))
+        mainPipeParal = partial(mainPipe, stackDict=stackPairDict, ilastikProj=ilastikProject, tempDir=tempPath, oDir=oDir)
+        # mainPipeParal(stackPairDict['s1'][0], 's1')
+        with Pool(mtNum) as p:
+            p.map(mainPipeParal, [posID for posID in stackPairDict])
+    else:
+        for posID in stackPairDict:
+            mainPipe(stackPairDict[posID][0], posID, ilastikProject, tempPath, oDir)
+             
 
-
-    splitImgs = unpackStack('./TestData/w1YFP-YM_s1.tiff', tempPath)
-    runIlastik('./TestData/pixelClass.ilp', splitImgs)
-    stackProb('./pepeline1_temp')
+    # splitImgs = unpackStack('./TestData/w1YFP-YM_s1.tiff', tempPath)
+    # runIlastik('./TestData/pixelClass.ilp', splitImgs)
+    # stackProb('./pepeline_temp', 's1', oDir)
     # print(splitImgs)
---
+++ Pipeline_SegProb.py
@@ -0,0 +1,6 @@
+from utils.ImgUtils import loadstack, save2stack
+
+
+if __name__ == "__main__":
+    
+    pass
