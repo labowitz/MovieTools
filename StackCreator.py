54bbf3d7746fd064efe0b01b69c2c502f57a35e4
yma2 <yitongma7@gmail.com>
2020-08-15 03:42:20 -0700

    Removed options for compression and normalization.

--- StackCreator.py
+++ StackCreator.py
@@ -2,6 +2,7 @@ import re
 import argparse
 import os.path as ospath
 import numpy as np
+import imageio
 from PIL import Image
 from os import listdir
 from os import mkdir
@@ -25,18 +26,17 @@ def normalizeImg(frame, maxI, perc):
 
     return normFrame
 
-def normalizeStack(imgStack):
-    stackArr = np.stack([np.array(im) for im in imgStack])
+# def normalizeStack(imgStack):
+#     stackArr = np.stack([np.array(im) for im in imgStack])
 
-    imgStack = []
-    for frame in stackArr:
-        normFrame = normalizeImg(frame, 65535, 1)
-        imgStack.append(Image.fromarray(normFrame.astype(np.uint16)))
-        # print(np.max(normFrame), np.min(normFrame))
-    return imgStack
+#     imgStack = []
+#     for frame in stackArr:
+#         normFrame = normalizeImg(frame, 65535, 1)
+#         imgStack.append(Image.fromarray(normFrame.astype(np.uint16)))
+#         # print(np.max(normFrame), np.min(normFrame))
+#     return imgStack
 
-def saveEachPosWave(multiParaInput):
-    pos, allImages, wDic, saveDic, compressOpt, normalize= multiParaInput
+def saveEachPosWave(pos, allImages, wDic, saveDic):
     posReg = re.compile(r'\w+_'+pos+'_')
     posImg = []
     for img in [imgName for imgName in allImages if posReg.match(imgName)]:
@@ -47,13 +47,13 @@ def saveEachPosWave(multiParaInput):
     
     imageFiles = []
     for timeP in posImg:
-        file = Image.open(wDic+'\\'+timeP)
+        file = imageio.imread(wDic+'/'+timeP)
         imageFiles.append(file)
 
-    if normalize:
-        imageFiles = normalizeStack(imageFiles)
-
-    imageFiles[0].save(saveDic+pos+'.tiff', compression=compressOpt, save_all=True, append_images=imageFiles[1:])
+    # if normalize:
+    #     imageFiles = normalizeStack(imageFiles)
+    imageio.mimsave(saveDic + pos + '.npz', imageFiles)
+    # imageFiles[0].save(saveDic+pos+'.tiff', compression=compressOpt, save_all=True, append_images=imageFiles[1:])
 
 
 if __name__ == '__main__':
@@ -63,9 +63,9 @@ if __name__ == '__main__':
     parser.add_argument("WorkDic", help="The directory containing all the images (only ends with .TIF)")
     parser.add_argument("-o", "--Output", help="The directory for stack output")    
     parser.add_argument("-t", "--MultiThread", type=int, default=1, help="Number of thread to use, default = 1")
-    parser.add_argument('--compressionOFF', action='store_true', help="Turn off compression if you are more CPU bound than IO bound (faster on a laptop with SSD). \
-    Of course it will increase final file sizes")
-    parser.add_argument('--normalize', action='store_true', help='Normalize across a each image using 1-99 percentile to 0~65535(16bit)')
+    # parser.add_argument('--compressionOFF', action='store_true', help="Turn off compression if you are more CPU bound than IO bound (faster on a laptop with SSD). \
+    # Of course it will increase final file sizes")
+    # parser.add_argument('--normalize', action='store_true', help='Normalize across a each image using 1-99 percentile to 0~65535(16bit)')
     args = parser.parse_args()
     
     # Handling parameters
@@ -78,10 +78,10 @@ if __name__ == '__main__':
         oDic = wDic
     print("Output to " + oDic)
 
-    compressOpt = 'tiff_deflate'
-    if args.compressionOFF:
-        print('No copression will be applied')
-        compressOpt = 'None'
+    # compressOpt = 'tiff_deflate'
+    # if args.compressionOFF:
+    #     print('No compression will be applied')
+    #     compressOpt = 'None'
 
     mtNum = args.MultiThread
 
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     else:
         multiP = True
 
-    normOpt = args.normalize
+    # normOpt = args.normalize
 
     # Making the list of images need to be processed
     allImages = [name for name in listdir(wDic) if name.endswith('.TIF')]
@@ -104,18 +104,18 @@ if __name__ == '__main__':
         positions.add(pos)
     
     # Make the folder
-    folderName = "NormedMovieStack" if normOpt else "RawMovieStack"
-    if not ospath.isdir(oDic+'\\{0}'.format(folderName)):
-        mkdir(oDic+'\\{0}'.format(folderName))
+    folderName = "RawMovieStack"
+    if not ospath.isdir(oDic+'/{0}'.format(folderName)):
+        mkdir(oDic+'/{0}'.format(folderName))
 
-    multiPara = list(zip(list(positions), [allImages]*len(positions), [wDic]*len(positions), [oDic+'\\{0}\\'.format(folderName)]*len(positions), [compressOpt]*len(positions), [normOpt]*len(positions)))
+    partialSaveFunc = partial(saveEachPosWave, allImages=allImages, wDic=wDic, saveDic=oDic+'/{0}/'.format(folderName))
 
     # Processing the images
     if multiP:
         print("Starting {0} threads......".format(mtNum))
         with Pool(mtNum) as p:
-            p.map(saveEachPosWave, multiPara)
+            p.map(partialSaveFunc, positions)
     else:
         print("Single thread mode")
-        for para in multiPara:
-            saveEachPosWave(para)
+        for pos in positions:
+            partialSaveFunc(pos)
